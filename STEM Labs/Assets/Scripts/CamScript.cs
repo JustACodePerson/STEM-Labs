01ea96619b6d844f625bbddac260c558a19e4a25
JustACodePerson <122057477+JustACodePerson@users.noreply.github.com>
2024-09-16 14:26:13 -0500

    Cam Movement

--- STEM Labs/Assets/Scripts/CamScript.cs
+++ STEM Labs/Assets/Scripts/CamScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CamScript : MonoBehaviour
 {
-    public Transform focalPoint;
-    void Start()
-    {
+    
+    [SerializeField] private float camMoveSpd; //Camera Movement Speed
+    [SerializeField] private float camRotSpd; //Camera Turning (Rotation) Speed
+    [SerializeField] private float scrollBounds; //Pixel Bounds for Edge Scrolling
+    
+    void Start(){
         
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        transform.LookAt(focalPoint);
+    void Update(){
+        //KEY-BASED MOVEMENT
+        Vector3 camDir = new Vector3(); //Camera Position Movement Vector
+        camDir += Input.GetAxis("Horizontal") * transform.right; //WS Movement
+        camDir += Input.GetAxis("Vertical") * transform.forward; //AD Movement
+
+        transform.position += camDir * camMoveSpd * Time.deltaTime; //Position Movement (WASD)
+        transform.eulerAngles += new Vector3(0, Input.GetAxis("Turn") * camRotSpd, 0); //Turn Movement (QE)
+        // VCam Body => X/Y Damping (Pos. Drag Delay), VCam Aim => Hor./Ver. Damping (Turn Drag Delay)
+
+        //EDGE SCROLLING
+        Vector3 mPos = new Vector3(); mPos = Input.mousePosition; //Mouse Position Tracker
+        //if(mPos)
     }
 }
