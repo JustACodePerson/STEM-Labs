b230f61253f2436cec015472e6009f0faeec40a3
JustACodePerson <lr040901@sdale.org>
2024-09-16 18:51:58 -0500

    Camera Mode Select

--- STEM Labs/Assets/Scripts/CamScript.cs
+++ STEM Labs/Assets/Scripts/CamScript.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class CamScript : MonoBehaviour
 {
-    
+    public enum MovementMode{
+        KeyCam,
+        FixedCam,
+        MouseCam
+    }
+    [Header("Cam Movement Opt")]
+    public MovementMode movementMode;
+    [Header("Cam Movement Stats")]
     [SerializeField] private float camMoveSpd; //Camera Movement Speed
     [SerializeField] private float camRotSpd; //Camera Turning (Rotation) Speed
     [SerializeField] private float scrollBounds; //Pixel Bounds for Edge Scrolling
     
     void Start(){
-        
     }
 
-    void Update(){
-        //KEY-BASED MOVEMENT
+    void Mode_KeyCam(){ //KEY-BASED MOVEMENT
         Vector3 camDir = new Vector3(); //Camera Position Movement Vector
         camDir += Input.GetAxis("Horizontal") * transform.right; //WS Movement
         camDir += Input.GetAxis("Vertical") * transform.forward; //AD Movement
@@ -26,5 +31,11 @@ public class CamScript : MonoBehaviour
         //EDGE SCROLLING
         Vector3 mPos = new Vector3(); mPos = Input.mousePosition; //Mouse Position Tracker
         //if(mPos)
+    }
+
+    void Update(){
+        if(movementMode == MovementMode.KeyCam){
+            Mode_KeyCam();
+        }
     }
 }
