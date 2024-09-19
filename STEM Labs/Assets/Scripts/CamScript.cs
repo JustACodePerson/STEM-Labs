7847e947453e0ea417bc4eba454d90b786c072c9
JustACodePerson <122057477+JustACodePerson@users.noreply.github.com>
2024-09-19 13:58:35 -0500

    Cam Changes

--- STEM Labs/Assets/Scripts/CamScript.cs
+++ STEM Labs/Assets/Scripts/CamScript.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CamScript : MonoBehaviour
 {
+    [Header("Reference Access")]
+    public CinemachineVirtualCamera vCam; //Access Cinemachine Cam
     
+    public enum CamView{ //Cam Viewing Mode
+        Top,
+        Bottom,
+        Front,
+        Back,
+        Left,
+        Right,
+        Roam
+    }
+    [Header("Camera Mode")]
+    public CamView currentCam = (CamView)0;
+    
+    [Header("Manipulable Stats")]
     [SerializeField] private float camMoveSpd; //Camera Movement Speed
     [SerializeField] private float camRotSpd; //Camera Turning (Rotation) Speed
     [SerializeField] private float scrollBounds; //Pixel Bounds for Edge Scrolling
     
+    private Vector3 camFollowOffset; //Access Cam Offset from Focal Point
+
     void Start(){
-        
+        camFollowOffset = vCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset;
     }
 
-    void Update(){
+    void camRoam(){
         //KEY-BASED MOVEMENT
         Vector3 camDir = new Vector3(); //Camera Position Movement Vector
         camDir += Input.GetAxis("Horizontal") * transform.right; //WS Movement
@@ -26,5 +44,25 @@ public class CamScript : MonoBehaviour
         //EDGE SCROLLING
         Vector3 mPos = new Vector3(); mPos = Input.mousePosition; //Mouse Position Tracker
         //if(mPos)
+
+        if(Input.GetKeyDown(KeyCode.R)){
+            camFollowOffset.y *= -1; //NEED TO FIX, REVERSE Y-VALUE OF TRANSPOSER BODY OF VCAM
+            Debug.Log("Hey");
+        }
+    }
+
+    void camFixed(){
+        //
+    }
+
+    void Update(){
+        camRoam();
+        /*
+        if(currentCam == (CamView)0){
+            camRoam();
+        }else{
+            camFixed();
+        }
+        */
     }
 }
