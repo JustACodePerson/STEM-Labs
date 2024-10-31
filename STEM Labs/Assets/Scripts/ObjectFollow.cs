8920a2af0cba885f9fa9a4d7da31d344f617ab77
JustACodePerson <122057477+JustACodePerson@users.noreply.github.com>
2024-10-31 14:25:14 -0500

    Rotate Object WIP

--- STEM Labs/Assets/Scripts/ObjectFollow.cs
+++ STEM Labs/Assets/Scripts/ObjectFollow.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class ObjectFollow : MonoBehaviour
 {
     private MeshRenderer mRend; //Access Renderer
+    private Transform tForm;
     private void Awake(){
-        mRend = this.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>(); //Access Object Color in Renderer
+        tForm = this.transform.GetChild(0).gameObject.GetComponent<Transform>(); //Access Object's Position/Rotation
+        mRend = this.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>(); //Access Object's Renderer
         mRend.material = Resources.Load<Material>("Materials/Mat_Active"); //Change Object to Transparent Color
     }
 
+    private void rotateObject(){
+        if (Input.GetKeyDown(KeyCode.LeftShift)){
+            if (Input.GetKeyDown(KeyCode.W)){
+                //tForm.transform.rotate(0,90,0); Rotate Object 90 DEG
+            }
+            if (Input.GetKeyDown(KeyCode.S)){
+            }
+            if (Input.GetKeyDown(KeyCode.A)){
+            }
+            if (Input.GetKeyDown(KeyCode.D)){
+            }
+        }
+    }
+
     private void Update(){
+        rotateObject();
+
         if (BuildSystem.current.gridToggle){ //Grid Building
             transform.position = BuildSystem.current.snapCoordToGrid(BuildSystem.mousePosGrid()); //Adjust Position to Grid
         }
         else{ //Non-Grid Building
-            transform.position = BuildSystem.mousePosObj(); // WIP: Adjust Position to Block
+            transform.position = BuildSystem.mousePosObj(); // WIP: ADJUST POSITION RELATIVE TO OTHER PLACED BLOCKS
         }
 
         if(Input.GetMouseButtonDown(0)){ //Left Click
             mRend.material = Resources.Load<Material>("Materials/Mat_Inactive"); //Change Object to Solid Color
-            //BuildSystem.InstObject(); // WIP: Make New Object
+            BuildSystem.current.InstObject(); // Make New Object When First Object is Placed
             Destroy(this); //Destroy Script
         }
         if(Input.GetMouseButtonDown(1)){ //Right Click
