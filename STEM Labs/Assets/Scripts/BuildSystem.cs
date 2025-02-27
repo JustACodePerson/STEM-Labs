afbb2ea18d4a08a6e622b82d167c5ce99bd1948e
JustACodePerson <122057477+JustACodePerson@users.noreply.github.com>
2025-02-27 13:49:40 -0600

    stuff

--- STEM Labs/Assets/Scripts/BuildSystem.cs
+++ STEM Labs/Assets/Scripts/BuildSystem.cs
@@ -11,6 +11,7 @@ public class BuildSystem : MonoBehaviour
     public GridLayout gridLayout; //Access Own-Script
     public bool gridToggle = true;
     public Transform parentObject; //Access Object the Prefabs Will Become Children Of
+    public Transform mainCam; //Access Object the Prefabs Will Become Children Of
     public GameObject prefabToInst; //Access Prefab that Will Be Instantiated
 
     private void Awake(){
@@ -22,7 +23,7 @@ public class BuildSystem : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //Create Line going thru Cam Center and 2D Mouse Pos on Screen
         
         if( Physics.Raycast( ray, out RaycastHit rcHitObj, LayerMask.GetMask("Object") ) ){ //If the Line Collides with an Object
-            //Debug.DrawRay(Vector3 start, Vector3 dir, Color.white, 1, true);
+            //Debug.DrawRay(current.mainCam.position, rcHitObj.point, Color.red, 1, true);
             return rcHitObj.point;// + rcHitObj.normal; //Give Offset Position of Collided Object (Object will be Held There)
         }
         else if( Physics.Raycast( ray, out RaycastHit rcHitGrid, LayerMask.GetMask("Grid") ) ){ //If the Line Collides with the Grid
